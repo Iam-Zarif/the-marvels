56be8f8dbf9fa1e458496fcf51a2863b618dffaf
Zarif <zarifraja19@gmail.com>
2023-06-17 12:06:54 +0600

    added

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -55,49 +55,56 @@ fetch("https://toy-marketplace-server-pied-psi.vercel.app/toys", {
     
   Title("Add Toys");
   return (
-    <div>
-        
+    <div className="mt-44">
       <div>
-      
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            
-            <div className="card flex-shrink-0   shadow-2xl bg-base-100">
-              <div className="card-body w-fit 2">
+        <h1 className="text-center font-bold text-4xl mb-10">Add Your Toy</h1>
+        <div className="hero py-10  bg-base-200">
+          <div className="grid grid-cols-1 lg:grid-cols-3">
+            <div className="">
+              <img className="mx-10"
+                src="https://i.ibb.co/y51LZrM/65c6ea9e407bc11243290d70fc0598f3.gif"
+                alt=""
+              />
+            </div>
+            <div className="card flex-shrink-0  col-span-2 shadow-2xl bg-base-100 mx-auto">
+              <div className="card-body w-fit ">
                 <form className="" onSubmit={addToys}>
                   <div className="grid grid-cols-1 gap-x-20 gap-y-3 lg:grid-cols-3">
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Picture URL of Toy</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="photo"
                         type="url"
                         placeholder="Provide the link"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Toy Name</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="name"
                         type="text"
                         placeholder="Give the Toy name"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Seller Name</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="seller"
                         value={user?.displayName}
                         type="text"
                         placeholder="Seller Name"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
@@ -105,7 +112,7 @@ fetch("https://toy-marketplace-server-pied-psi.vercel.app/toys", {
                         <span className="label-text">Category</span>
                       </label>
                       <select
-                        className="select select-info w-96 block input-group"
+                        className="select input-bordered input-warning input-warning w-64 block input-group"
                         name="Category"
                       >
                         <option className="input">Iron-man</option>
@@ -117,62 +124,69 @@ fetch("https://toy-marketplace-server-pied-psi.vercel.app/toys", {
                       <label className="label">
                         <span className="label-text">Price</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="price"
                         type="number"
                         placeholder="Price $"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Rating</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="rating"
                         type="text"
                         placeholder="share your Rating"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Available Quantity</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="quantity"
                         type="number"
                         placeholder="How much you want it"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                     <div className="form-control">
                       <label className="label">
-                        <span className="label-text">Details description (optional)</span>
+                        <span className="label-text">
+                          Details description (optional)
+                        </span>
                       </label>
-                      <textarea 
+                      <textarea
                         name="description"
                         placeholder="description"
-                        className="textarea textarea-bordered textarea-lg w-full "
+                        className="textarea border-warning textarea-bordered textarea-lg w-full "
                       ></textarea>
                     </div>
                     <div className="form-control">
                       <label className="label">
                         <span className="label-text">Seller email</span>
                       </label>
-                      <input required
+                      <input
+                        required
                         name="email"
                         type="email"
                         value={user?.email}
                         placeholder="Give seller's email"
-                        className="w-60 lg:w-96 input input-bordered"
+                        className="w-60 lg:w-64 input input-bordered input-warning"
                       />
                     </div>
                   </div>
                   <div className="form-control mt-6">
-                    <button 
-                    onClick={success}
-                    className=" text-lg text-yellow-500 ToyBtn border rounded-xl py-2 mt-4 hover:border-yellow-500">
+                    <button
+                      onClick={success}
+                      className=" text-lg text-yellow-500 ToyBtn border rounded-xl py-2 mt-4 hover:border-yellow-500"
+                    >
                       Add Toy
                     </button>
                   </div>
--- src/Pages/Home/MyToys/MyToysItems.jsx
+++ src/Pages/Home/MyToys/MyToysItems.jsx
@@ -6,32 +6,7 @@ import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const MyToysItems = ({ data, deleteMe }) => {
-  //   const deleteMe = (_id) => {
-  //     console.log(_id);
-  //     Swal.fire({
-  //       title: "Are you sure?",
-  //       text: "You won't be able to revert this!",
-  //       icon: "warning",
-  //       showCancelButton: true,
-  //       confirmButtonColor: "#3085d6",
-  //       cancelButtonColor: "#d33",
-  //       confirmButtonText: "Yes, delete it!",
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         fetch(`https://toy-marketplace-server-pied-psi.vercel.app/myToys/${_id}`,{
-  //             method :"DELETE"
-  //         })
-  //           .then((res) => res.json())
-  //           .then((data) => {
-  //             console.log(data);
-  //             if (data.deleteCount > 0) {
-  //               Swal.fire("Deleted!", "Your Toy data has been deleted.", "success");
-
-  //             }
-  //           });
-  //       }
-  //     });
-  //   };
+ 
 
   const {
     _id,
@@ -56,7 +31,7 @@ const MyToysItems = ({ data, deleteMe }) => {
             {/* row 1 */}
             <tr className=" hover items-center text-lg grid grid-cols-7">
               <td>
-                <img src={photo} className="w-20 rounded-full" alt="" />
+                <img src={photo} className="w-[60px] h-[60px] rounded-full" alt="" />
               </td>
               <td>{name}</td>
               <td>{Category}</td>
